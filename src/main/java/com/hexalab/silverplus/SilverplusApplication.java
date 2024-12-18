508d5b13e9eed55234b5fb774153bfff187c6877
최은영 <dev.choiey@gmail.com>
2024-12-18 12:32:00 +0900

    add: .gitignore

--- src/main/java/com/hexalab/silverplus/SilverplusApplication.java
+++ src/main/java/com/hexalab/silverplus/SilverplusApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SilverplusApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SilverplusApplication.class, args);
     }
