42be27c5e043f18c431ae07a8f281eba88f60480
HongSaeKim <84121339+ghdtpwlq@users.noreply.github.com>
2024-12-19 17:00:32 +0900

    fix
    누락데이터

--- src/main/java/com/hexalab/silverplus/qna/jpa/repository/QnARepository.java
+++ src/main/java/com/hexalab/silverplus/qna/jpa/repository/QnARepository.java
@@ -2,9 +2,9 @@ package com.hexalab.silverplus.qna.jpa.repository;
 
 import com.hexalab.silverplus.qna.jpa.entity.QnAEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
-public interface QnARepository extends JpaRepository<QnAEntity, UUID> {
+@Repository
+public interface QnARepository extends JpaRepository<QnAEntity, String>, QnARepositoryCustom {
 
 }
