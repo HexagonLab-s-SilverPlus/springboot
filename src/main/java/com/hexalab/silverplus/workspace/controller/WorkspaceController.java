ca26eacc35f47acd30024f362cd9d061b1ea76c8
최은영 <dev.choiey@gmail.com>
2024-12-26 11:48:27 +0900

    feat: 삭제되지 않은, 삭제된 워크스페이스 목록 조회 컨트롤러 메소드 구현

--- src/main/java/com/hexalab/silverplus/workspace/controller/WorkspaceController.java
+++ src/main/java/com/hexalab/silverplus/workspace/controller/WorkspaceController.java
@@ -21,10 +21,15 @@ public class WorkspaceController {
     @Autowired
     private WorkspaceService workspaceService;
 
+    /**
+     * 삭제되지 않은 워크스페이스 조회 (기본 조회로 설정)
+     * @param memUuid
+     * @return
+     */
     @GetMapping("/{memUuid}")
-    public ResponseEntity<ApiResponse<List<Workspace>>> getWorkspace(@PathVariable String memUuid) {
+    public ResponseEntity<ApiResponse<List<Workspace>>> getWorkspacesNotDeleted(@PathVariable String memUuid) {
         try {
-            List<Workspace> workspaces = workspaceService.getWorkspaceByMemUuid(memUuid);
+            List<Workspace> workspaces = workspaceService.getWorkspacesNotDeleted(memUuid);
             if (workspaces.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.<List<Workspace>>builder()
                         .success(false)
@@ -41,6 +46,32 @@ public class WorkspaceController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.<List<Workspace>>builder()
                     .success(false)
                     .message("워크스페이스 조회 실패")
+                    .build());
+        }
+    }
+
+
+    @GetMapping("/deleted/{memUuid}")
+    public ResponseEntity<ApiResponse<List<Workspace>>> getDeletedWorkspaces(@PathVariable String memUuid){
+        try{
+            List<Workspace> workspaces = workspaceService.getDeletedWorkspaces(memUuid);
+            if(workspaces.isEmpty()){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.<List<Workspace>>builder()
+                        .success(false)
+                        .message("삭제된 워크스페이스가 존재하지 않습니다.")
+                        .build());
+            }
+            return ResponseEntity.ok(ApiResponse.<List<Workspace>>builder()
+                    .success(true)
+                    .message("삭제된 워크스페이스 조회 성공")
+                    .data(workspaces)
+                    .build());
+        }catch(Exception e){
+            e.printStackTrace();
+            log.error("삭제된 워크스페이스 조회 실패:", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.<List<Workspace>>builder()
+                    .success(false)
+                    .message("삭제된 워크스페이스 조회 실패")
                     .build());
         }
     }
