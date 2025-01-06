ad74ac87d3d8dd8f3bff0de4f0da1d5442dca82c
SangMoo Lee <dltkdan7373@hanmail.net>
2025-01-06 20:51:51 +0900

    feat . 전자책 리스트

--- src/main/java/com/hexalab/silverplus/book/jpa/repository/BookRepositoryCustom.java
+++ src/main/java/com/hexalab/silverplus/book/jpa/repository/BookRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.hexalab.silverplus.book.jpa.repository;
 
+import com.hexalab.silverplus.book.jpa.entity.BookEntity;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface BookRepositoryCustom {
+    long selectSearchBookListCount(String keyword);
+    List<BookEntity> selectSearchBookList(String keyword, Pageable pageable);
 }
--- src/main/java/com/hexalab/silverplus/book/jpa/repository/BookRepositoryCustomImpl.java
+++ src/main/java/com/hexalab/silverplus/book/jpa/repository/BookRepositoryCustomImpl.java
@@ -1,11 +1,42 @@
 package com.hexalab.silverplus.book.jpa.repository;
 
+import com.hexalab.silverplus.book.jpa.entity.BookEntity;
+import com.hexalab.silverplus.book.jpa.entity.QBookEntity;
+import com.hexalab.silverplus.notice.jpa.entity.QNoticeEntity;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class BookRepositoryCustomImpl implements BookRepositoryCustom {
+    // QueryDSL 에 대한 config 클래스를 먼저 만들고 나서 필드 선언함
+    private final JPAQueryFactory queryFactory;
+    // JPQL 사용을 위한 의존성 주입
+    private final EntityManager entityManager;
+    private QBookEntity book = QBookEntity.bookEntity;
 
-
+    // 북 검색 리스트 갯수 조회
+    @Override
+    public long selectSearchBookListCount(String keyword) {
+        return queryFactory
+                .selectFrom(book)
+                .where(book.bookTitle.like("%"+keyword+"%"))
+                .fetchCount();
+    }
+    // 북 검색 리스트
+    @Override
+    public List<BookEntity> selectSearchBookList(String keyword, Pageable pageable) {
+        return queryFactory
+                .selectFrom(book)
+                .where(book.bookTitle.like("%"+keyword+"%"))
+                .orderBy(book.bookCreateAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
 }
