1079dd1abd431a183b209c95683bfae8aa67a2b4
최은영 <dev.choiey@gmail.com>
2025-01-06 19:41:02 +0900

    feat: DocFile 데이터 저장 성공

--- src/main/java/com/hexalab/silverplus/document/model/service/DocFileService.java
+++ src/main/java/com/hexalab/silverplus/document/model/service/DocFileService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -41,24 +42,49 @@ public class DocFileService {
     private String ftpRemoteDir;
 
     @Transactional
-    public DocFile saveDocFile(String docId, MultipartFile file) throws IOException {
+    public DocFile saveDocFile(String docId, MultipartFile file, String csvFilename) throws IOException {
         String originalFilename = file.getOriginalFilename();
-        String renamedFilename = UUID.randomUUID() + "_" + originalFilename;
+        // Flask에서 받은 csvFilename을 그대로 사용
+        String renamedFilename = csvFilename;
 
+        // 임시파일 생성 및 NAS 업로드
+        // docfile-1234567890.tmp (접두사 뒤에 시스템에서 고유한 임의의 문자열 추가됨)
+        // suffix(확장자)가 null이면 기본 확장자인 ".tmp"가 사용된다.
+        // File.createTempFile은 운영체제가 제공하는 기본 임시 디렉토리에 파일을 생성한다.
+        //Windows: C:\Users\<사용자>\AppData\Local\Temp
+
+        File tempFile = File.createTempFile("docfile-", null);
         // NAS 업로드
         try {
+            // MultipartFile을 임시 파일로 저장
+            file.transferTo(tempFile);
             ftPUtility.connect(ftpServer, ftpPort, ftpUsername, ftpPassword);
-            String remoteFilePath = ftpRemoteDir + renamedFilename;
-            ftPUtility.uploadFile(file.getInputStream().toString(), remoteFilePath);
+            String remoteFilePath = ftpRemoteDir + "document/"+renamedFilename;
+            // 임시 파일 생성 -> 메모리에만 존재하는 MultipartFile을 물리적인 파일로 변환해 FTP 업로드에 활용할 수 있다.
+            // temp.getAbsolutePath()를 FTP 업로드 메소드에 전달한다,.
+            //macOS/Linux: /tmp
+            ftPUtility.uploadFile(tempFile.getAbsolutePath(), remoteFilePath);
             log.info("NAS에 파일 업로드 완료: {}", remoteFilePath);
-        } finally {
+        }catch (Exception e){
+            e.printStackTrace();
+            log.error("파일 업로드 중 오류 발생: {}", e.getMessage());
+            throw e;
+        }
+        finally {
+            // FTP 연결 종료
             ftPUtility.close();
+            // FTP 업로드가 완료되면 더 이상 필요치 않은 임시 파일은 삭제
+            // 로컬 스토리지 리소스 확보
+            if(tempFile.exists()){
+                tempFile.delete();
+                log.info("임시파일 삭제 완료: {}", tempFile.getAbsoluteFile());
+            }
         }
 
         // DB 저장
         DocFileEntity docFileEntity = DocFileEntity.builder()
                 .dfId(UUID.randomUUID().toString())
-                .dfOriginalName(originalFilename)
+                .dfOriginalName(originalFilename) // 실제 업로드된 원본 파일명
                 .dfRename(renamedFilename)
                 .docId(docId)
                 .build();
