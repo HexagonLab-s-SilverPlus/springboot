6cf6fa1c8b6c4e2dc7abc4e49c61f7d7d1213944
최은영 <dev.choiey@gmail.com>
2025-01-08 17:15:33 +0900

    feat: FTPUtility로 나스의 파일 다운로드 구현

--- src/main/java/com/hexalab/silverplus/document/controller/DocFileController.java
+++ src/main/java/com/hexalab/silverplus/document/controller/DocFileController.java
@@ -1,14 +1,22 @@
 package com.hexalab.silverplus.document.controller;
 
 import com.hexalab.silverplus.common.ApiResponse;
+import com.hexalab.silverplus.common.FTPUtility;
 import com.hexalab.silverplus.document.model.dto.DocFile;
 import com.hexalab.silverplus.document.model.service.DocFileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @RestController
@@ -19,6 +27,21 @@ import java.io.IOException;
 public class DocFileController {
 
     private final DocFileService docFileService;
+
+
+
+    // file upload path valiable
+    @Value("${ftp.server}")
+    private String ftpServer;
+    @Value("${ftp.port}")
+    private int ftpPort;
+    @Value("${ftp.username}")
+    private String ftpUsername;
+    @Value("${ftp.password}")
+    private String ftpPassword;
+    @Value("${ftp.remote-dir}")
+    private String ftpRemoteDir;
+
 
     /**
      * 공문서 파일 저장
@@ -49,6 +72,55 @@ public class DocFileController {
             );
         }
     }
+
+
+
+
+
+
+    /**
+     * FTP를 통해 파일 다운로드
+     *
+     * @param fileName 다운로드할 파일명
+     * @return 파일 바이너리 데이터
+     */
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<Resource> downloadFileFromFTP(@PathVariable String fileName) {
+        try (FTPUtility ftpUtility = new FTPUtility()) {
+            // FTP 서버 연결
+            ftpUtility.connect(ftpServer, ftpPort, ftpUsername, ftpPassword);
+
+            // 로컬 임시 파일 생성
+            File tempFile = File.createTempFile("ftp_download_", "_" + fileName);
+
+            // FTP에서 파일 다운로드
+            ftpUtility.downloadFile(ftpRemoteDir + "document/" + fileName, tempFile.getAbsolutePath());
+
+            // 파일 데이터를 Resource로 변환
+            Resource resource = new InputStreamResource(new FileInputStream(tempFile));
+
+            // HTTP 헤더 설정
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"");
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+            // 임시 파일 삭제
+            tempFile.delete();
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(resource);
+        } catch (IOException e) {
+            log.error("FTP 파일 다운로드 실패: {}", e.getMessage());
+            return ResponseEntity.status(500).body(null);
+        }
+    }
+
+
+
+
+
+
 
 
     /**
